2d8a02a16d2ec2c7ab80a6f5825fcef58f9aae8b
einfachder <einfachder@gmx.de>
2018-09-17 16:52:44 +0200

    Beta
    Grundgerüst des Schachspiels
    
    Co-Authored-By: Koric <koric@users.noreply.github.com>
    Co-Authored-By: einfachder <einfachder@users.noreply.github.com>

---
+++ Assets/Scripts/Highlights.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlights : MonoBehaviour {
+
+	public static Highlights Instance{get;set;}
+
+	public GameObject highlightPrefab;
+	private List<GameObject> highlights;
+	private void Start()
+	{
+		Instance = this;
+		highlights = new List<GameObject> ();
+
+	}
+
+	private GameObject GetHighlightObject()
+	{
+		GameObject gmeObj = highlights.Find(g => !g.activeSelf);
+
+			if(gmeObj == null)
+			{
+				gmeObj = Instantiate (highlightPrefab);
+				highlights.Add(gmeObj);
+			}
+			return gmeObj;
+	}
+
+	public void HighlightAllowedMoves(bool[,] moves)
+	{
+		for (int i=0; i<8; i++)
+		{
+			for (int j=0; j<8; j++)
+			{
+				if(moves[i,j] == true)
+				{
+					GameObject gmeObj = GetHighlightObject();
+					gmeObj.SetActive(true);
+					gmeObj.transform.position = new Vector3(i+0.5f,0,j+0.5f);
+				}
+			}
+		}
+	}
+
+	public void Hide()
+	{
+		foreach (GameObject gmeObj in highlights)
+		{
+			gmeObj.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Koenig.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koenig : Figur {
+
+    public override bool[,] PossibleMove()
+    {
+        bool[,] arr = new bool[8,8];
+        Figur fig;
+
+        //links
+        if(x-1 >= 0)
+        {
+            fig = BoardController.Instance.figures[x-1,y];
+            if(fig == null || fig.isWhite != isWhite)
+            {
+                arr[x-1,y] = true;
+            }
+        }
+
+        //rechts
+        if(x+1 < 8)
+        {
+            fig = BoardController.Instance.figures[x+1,y];
+            if(fig == null || fig.isWhite != isWhite)
+            {
+                arr[x+1,y] = true;
+            }
+        }
+
+        //oben
+        if(y+1 < 8)
+        {
+            fig = BoardController.Instance.figures[x,y+1];
+            if(fig == null || fig.isWhite != isWhite)
+            {
+                arr[x,y+1] = true;
+            }
+
+            //oben links
+            if(x-1 >= 0)
+            {
+                fig = BoardController.Instance.figures[x-1,y+1];
+                if(fig == null || fig.isWhite != isWhite)
+                {
+                    arr[x-1,y+1] = true;
+                }
+            }
+
+            //oben rechts
+            if(x+1 < 8)
+            {
+                fig = BoardController.Instance.figures[x+1,y+1];
+                if(fig == null || fig.isWhite != isWhite)
+                {
+                    arr[x+1,y+1] = true;
+                }
+            }
+        }
+
+        //unten
+        if(y-1 >= 0)
+        {
+            
+            fig = BoardController.Instance.figures[x,y-1];
+            if(fig == null || fig.isWhite != isWhite)
+            {
+                arr[x,y-1] = true;
+            }
+
+            //unten links
+            if(x-1 >= 0)
+            {
+                fig = BoardController.Instance.figures[x-1,y-1];
+                if(fig == null || fig.isWhite != isWhite)
+                {
+                    arr[x-1,y-1] = true;
+                }
+            }
+
+            //unten rechts
+            if(x+1 < 8)
+            {
+                fig = BoardController.Instance.figures[x+1,y-1];
+                if(fig == null || fig.isWhite != isWhite)
+                {
+                    arr[x+1,y-1] = true;
+                }
+            }
+        }
+        return arr;
+    }
+}
